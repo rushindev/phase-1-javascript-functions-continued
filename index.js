84b85c7f061d981c40b6a395f04761511c47263f
York Rushin <rushinyork@gmail.com>
2022-06-09 22:03:46 -0700

    commit tree 36c6a750a97153f3074b94d57737416777af3c1d

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+let funActivity
+let boringWork
+function saturdayFun(activity) {
+    if (typeof activity === 'undefined') {
+        funActivity = 'roller-skate';
+    } else funActivity = activity
+    return `This Saturday, I want to ${funActivity}!`
+}
+
+function mondayWork(work) {
+    if (typeof work === 'undefined') {
+        boringWork = 'go to the office';
+    } else boringWork = work
+    return `This Monday, I will ${boringWork}.`
+}
+
+function wrapAdjective(special, adjective) {
+    return `You are ${special}${adjective}${special}!`
+}
